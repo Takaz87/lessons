4913b1f27f6731f43e5d1d3f2e731432db639529
Takaz87 <149719564+Takaz87@users.noreply.github.com>
2024-02-07 20:59:34 +0300

    add lesson-76

---
+++ lesson-76/main.js
@@ -0,0 +1,9 @@
+const firstName = 'Aslan'
+let age = 36
+const lastName = 'Takazov'
+const myBirthday = '28.02.1987'
+const myHeight = '176'
+
+
+console.log(firstName, lastName, age, "y.o.", myBirthday, myHeight)
+
