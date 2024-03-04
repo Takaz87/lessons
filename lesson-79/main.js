937089d69b37f32ae99299fb6e7135da7f6018ee
Takaz87 <149719564+Takaz87@users.noreply.github.com>
2024-03-04 19:52:56 +0300

    add lesson-79

---
+++ lesson-79/main.js
@@ -0,0 +1,11 @@
+// const firstNumber = 18
+// const secondNumber = 37
+
+
+// if (firstNumber <= secondNumber) {
+//   const result = 'Вы совершеннолетний'
+//   console.log(result)
+// }
+const age = '37'
+age === '37' ? console.log('Вы совершеннолетний') : console.log('Несовершеннолетний')
+
